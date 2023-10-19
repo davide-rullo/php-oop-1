a41ba133e3b7fa37f740b2dfbf1eb73a3c46b1d9
davide-rullo <davide.rullo01@gmail.com>
2023-10-19 12:59:21 +0200

    add movies properties in html file

--- index.php
+++ index.php
@@ -4,11 +4,30 @@ class Movie
     public $title;
     public $year;
 
-    function __construct($_year)
+    function __construct($_title, $_year)
     {
+        $this->title = $_title;
         $this->year = $_year;
     }
 }
 
-$Oppenheimer = new Movie(2023);
-$Avatar = new Movie(2011);
+$movie1 = new Movie('Oppenheimer', '2023');
+$movie2 = new Movie('Avatar', '2011');
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h2><?php echo $movie1->title . ' ' . $movie1->year ?></h2>
+    <h2><?php echo $movie2->title . ' ' . $movie2->year ?></h2>
+</body>
+
+</html>
